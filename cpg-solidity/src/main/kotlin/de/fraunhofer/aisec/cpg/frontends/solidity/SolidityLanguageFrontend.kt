aa06707bfefbb341eec2e06cbfe565db5a30d5fb
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2021-10-12 16:13:27 +0200

    Add code and region computation

--- cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/solidity/SolidityLanguageFrontend.kt
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/frontends/solidity/SolidityLanguageFrontend.kt
@@ -4,13 +4,18 @@ import SolidityLexer
 import SolidityParser
 import de.fraunhofer.aisec.cpg.TranslationConfiguration
 import de.fraunhofer.aisec.cpg.frontends.LanguageFrontend
+import de.fraunhofer.aisec.cpg.graph.TypeManager
 import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
 import de.fraunhofer.aisec.cpg.passes.scopes.ScopeManager
 import de.fraunhofer.aisec.cpg.sarif.PhysicalLocation
+import de.fraunhofer.aisec.cpg.sarif.Region
 import org.antlr.v4.runtime.ANTLRInputStream
-import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.ParserRuleContext
+import org.antlr.v4.runtime.Token
+import org.antlr.v4.runtime.misc.Interval
 import org.checkerframework.checker.nullness.qual.NonNull
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileInputStream
 
@@ -18,15 +23,22 @@ class SolidityLanguageFrontend(
     config: @NonNull TranslationConfiguration,
     scopeManager: ScopeManager?,
 ) : LanguageFrontend(config, scopeManager, ".") {
+
+    private val logger = LoggerFactory.getLogger(SolidityLanguageFrontend.javaClass)
+
     val statementHandler = StatementHandler(this)
     val expressionHandler = ExpressionHandler(this)
     val typeHandler = TypeHandler(this)
     val declarationHandler = DeclarationHandler(this)
+    var currentFile: File? = null;
 
     override fun parse(file: File): TranslationUnitDeclaration {
+        TypeManager.getInstance().setLanguageFrontend(this)
+
         val lexer = SolidityLexer(ANTLRInputStream(FileInputStream(file)))
         val stream = CommonTokenStream(lexer)
         val parser = SolidityParser(stream)
+        currentFile = file
 
         return handleSourceUnit(parser.sourceUnit())
     }
@@ -54,10 +66,30 @@ class SolidityLanguageFrontend(
     }
 
     override fun <T : Any?> getCodeFromRawNode(astNode: T): String? {
+        if(astNode is ParserRuleContext){
+            return astNode.start.inputStream.getText(Interval(astNode.start.startIndex, astNode.stop.stopIndex))
+        }else{
+            astNode?.let{
+                logger.warn("Code was not added as the parsed Object {} is not of type {}", it::class.simpleName, ParserRuleContext::class.simpleName)
+            }
+        }
         return null
     }
 
     override fun <T : Any?> getLocationFromRawNode(astNode: T): PhysicalLocation? {
+        if(astNode is ParserRuleContext){
+            val startToken: Token = astNode.start
+            val stopToken: Token = astNode.stop
+            val region = Region(startToken.line,
+                startToken.charPositionInLine + 1,
+                stopToken.line,
+                stopToken.charPositionInLine + stopToken.text.length + 1) // + 1 for sarif format
+            return PhysicalLocation(currentFile!!.toURI(), region)
+        }else{
+            astNode?.let{
+                logger.warn("PhysicalLocation was not added as the parsed Object {} is not of type {}", it::class.simpleName, ParserRuleContext::class.simpleName)
+            }
+        }
         return null
     }
 
