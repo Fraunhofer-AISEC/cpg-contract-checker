8a61127f2ef37f223e39844d5318ac0d77ad6360
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2022-06-14 10:58:39 +0200

    Addign reentrancy restriction to exclude calls with bases coming from contract constants and constructor parameters

--- cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/ReentrancyCheck.kt
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/ReentrancyCheck.kt
@@ -5,7 +5,7 @@ import org.neo4j.driver.Transaction
 
 class ReentrancyCheck: Check() {
     override fun check(transaction: Transaction): Boolean {
-        val baseReentrency = "match p=(b)-[:BASE]-(c:CallExpression)-[e:EOG*]->(n)-[d:DFG*]->(ex:Expression)-[g]->()-[:REFERS_TO]->(:FieldDeclaration) where not type(g) in ['EOG','DFG'] return c"
+        val baseReentrency = "match p=(s)-[:DFG*]->(b)-[:BASE]-(c:CallExpression)-[e:EOG*]->(n)-[d:DFG*]->(ex:Expression)-[g]->()-[:REFERS_TO]->(:FieldDeclaration) where not type(g) in ['EOG','DFG'] and not exists (()-[:DFG]->(s)) and not 'Literal' in labels(s) and (not 'ParamVariableDeclaration' in labels(s)or exists((s)--(:ConstructorDeclaration)))return c"
         transaction.run(baseReentrency).let { result ->
             while (result.hasNext()) {
                 val row: Record = result.next()
