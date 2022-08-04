f0940d1e58c43820b32c33ae605680885fd6ab3f
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2022-08-04 13:47:18 +0200

    Adding Address before paddable value requirement fixing some mistakes

--- cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/AddressPaddingCheck.kt
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/AddressPaddingCheck.kt
@@ -12,29 +12,30 @@ class AddressPaddingCheck : Check() {
     override fun check(transaction: Transaction): List<PhysicalLocation> {
         var findings: MutableList<PhysicalLocation> = mutableListOf()
         val query =
-            "match p=(f:FunctionDeclaration)-[:EOG|INVOKES*]->(c:CallExpression)-[:EOG|INVOKES*]->(last)<--(f)\n" +
-                    "where (toUpper(c.name) in ['TRANSFER' , 'SEND']\n" +
-                    "    and exists{\n" +
-                    "        (f)-[r:PARAMETERS]->(param:ParamVariableDeclaration)-[:DFG*]->()<-[:ARGUMENTS]-(v)\n" +
-                    "        where not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX}\n" +
-                    "    }\n" +
-                    "    or exists{\n" +
-                    "        (f)-[r:PARAMETERS]->(param:ParamVariableDeclaration)-[:DFG*]->()<-[:VALUE]-(s)-[:KEY]->({value: 'value'})\n" +
-                    "            where exists{(s)<--(c)} and not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX}\n" +
+            "match p=({name: \"address\"})<-[:TYPE]-(ad)<-[adr:PARAMETERS]-(f:FunctionDeclaration)-[:EOG|INVOKES*]->(c:CallExpression)-[:EOG|INVOKES*]->(last)<--(f)\n" +
+                    "    where (toUpper(c.name) in ['TRANSFER' , 'SEND']\n" +
+                    "        and exists{\n" +
+                    "            (f)-[r:PARAMETERS]->(param:ParamVariableDeclaration)-[:DFG*]->()<-[:ARGUMENTS]-(c)\n" +
+                    "            where not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX} and adr.INDEX < r.INDEX\n" +
+                    "            }\n" +
+                    "        }\n" +
+                    "        or exists{\n" +
+                    "            (f)-[r:PARAMETERS]->(param:ParamVariableDeclaration)-[:DFG*]->()<-[:VALUE]-(s)-[:KEY]->({value: 'value'})\n" +
+                    "            where exists{(s)<--(c)} and not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX} and adr.INDEX < r.INDEX\n" +
                     "        }\n" +
                     "        or toUpper(c.name) in ['VALUE'] and exists{\n" +
                     "            (f)-[r:PARAMETERS]->(param:ParamVariableDeclaration)-[:DFG*]->()<-[:ARGUMENTS]-(c)-[:BASE*]->({name:'call'})\n" +
-                    "            where not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX}\n" +
+                    "            where not exists {(f)-[rp:PARAMETERS]->() where rp.INDEX > r.INDEX} and adr.INDEX < r.INDEX\n" +
                     "        })\n" +
                     "    and not exists{\n" +
-                    "        ({code : 'msg.data.length'})-[:DFG*]->(n) \n" +
+                    "        ({code : 'msg.data.length'})-[:DFG*]->(n)\n" +
                     "        where n in nodes(p)\n" +
                     "        and exists{alt=(n)-[:EOG|INVOKES*]->(t) where not 'ROLLBACK' in labels(t) and not c in nodes(alt)}\n" +
-                    "    }\n" +
-                    "    and exists{\n" +
-                    "        (c)-[:BASE*]->()<-[:DFG*]-(:ParamVariableDeclaration)\n" +
-                    "    }\n" +
-                    "return c as call, c.startLine as sline, c.endLine as eline, c.startColumn as scol, c.endColumn as ecol, c.artifact as file"
+                    "                        }\n" +
+                    "        and exists{\n" +
+                    "            (c)-[:BASE*]->()<-[:DFG*]-(:ParamVariableDeclaration)\n" +
+                    "        }\n" +
+                    "return distinct c as call, c.startLine as sline, c.endLine as eline, c.startColumn as scol, c.endColumn as ecol, c.artifact as file"
 
         transaction.run(query).let { result ->
             while (result.hasNext()) {
