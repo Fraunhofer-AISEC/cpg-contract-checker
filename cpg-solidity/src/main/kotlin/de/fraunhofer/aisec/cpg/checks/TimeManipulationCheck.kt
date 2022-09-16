b695186df3bee60e4fa62c9801e790e481e3bc4e
Christof Torres <christoftorres@users.noreply.github.com>
2022-09-16 09:13:19 +0200

    Create TimeManipulationCheck.kt

---
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/TimeManipulationCheck.kt
@@ -0,0 +1,25 @@
+package de.fraunhofer.aisec.cpg.checks
+
+import de.fraunhofer.aisec.cpg.sarif.PhysicalLocation
+import org.neo4j.driver.Record
+import org.neo4j.driver.Transaction
+
+class TimeManipulationCheck: Check() {
+
+    override fun getVulnerabilityName(): String {
+        return "Time Manipulation Vulnerability"
+    }
+
+    override fun check(transaction: Transaction): List<PhysicalLocation> {
+        var findings: MutableList<PhysicalLocation> = mutableListOf()
+        val query = ""
+        transaction.run(query).let { result ->
+            while (result.hasNext()) {
+                val row: Map<String, Any> = result.next().asMap()
+                findings.add(getPhysicalLocationFromResult(row))
+
+            }
+        }
+        return findings
+    }
+}
