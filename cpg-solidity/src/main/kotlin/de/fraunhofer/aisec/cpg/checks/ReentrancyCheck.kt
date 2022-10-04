bf74060ba05e80a68e1dc9fcbd87e74ec3410084
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2022-10-04 20:49:47 +0200

    Reentrancy check enhanced by a mutex detection to eliminate false positives

--- cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/ReentrancyCheck.kt
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/ReentrancyCheck.kt
@@ -20,7 +20,21 @@ class ReentrancyCheck: Check() {
                 "or exists {\n" +
                 "    (s)-[:DFG*]->(b2)<-[:BASE*]-(c)\n" +
                 "    where not exists (()-[:DFG]->(s)) and  not 'Literal' in labels(s) and not  exists((s)<-[:PARAMETERS]-(:ConstructorDeclaration)) and (not s.isInferred or s.name in ['msg', 'tx'] )\n" +
-                "})\n" +
+                "}) and not exists{\n" +
+                "    r=(cd)-[:EOG*]->(l1:Literal)-[:EOG*]->()-[:EOG*]->(l2:Literal)\n" +
+                "where l1.value <> l2.value and exists{(l1:Literal)-[:DFG*]->(f:FieldDeclaration)<-[:DFG*]-(l2:Literal)} and (cd in nodes(p) and n in nodes(r) or l2 in nodes(r) and c in nodes(p) or c in nodes(r) and n in nodes(r) and l2 in nodes(r)) and\n" +
+                "    exists{ q=(cd)-[:EOG*]->(pathend) where not exists ((pathend)-[:EOG]->()) and all(altn in nodes(q) where altn = cd or not altn in nodes(r) )\n" +
+                "    and (exists {\n" +
+                "        (f)<-[:REFERS_TO]-({access: 'READ'})<--(bin:BinaryOperator {operatorCode: '=='})-->(cl:Literal) where cl.value = l2.value and exists ((bin)-[:DFG]->(cd)) \n" +
+                "    }or not exists ((:BinaryOperator)-[:DFG]->(cd)) and (exists {\n" +
+                "        (f)<-[:REFERS_TO]-({access: 'READ'})-[:DFG]->(un:UnaryOperator)-[:DFG]->(cd) where un.operatorCode = '!' and l1.value = true\n" +
+                "    }\n" +
+                "    or not exists ((:UnaryOperator)-[:DFG]->(cd)) and exists {\n" +
+                "        (f)<-[:REFERS_TO]-({access: 'READ'})-[:DFG*]->(cd) where l1.value = false\n" +
+                "    })\n" +
+                "    )\n" +
+                "}\n" +
+                "}\n" +
                 "return distinct  c as call, c.startLine as sline, c.endLine as eline, c.startColumn as scol, c.endColumn as ecol, c.artifact as file, c.name as name"
         var findings: MutableList<PhysicalLocation> = mutableListOf()
             transaction.run(baseReentrency).let { result ->
