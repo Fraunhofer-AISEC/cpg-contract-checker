9a3f18723915e618ec4192473ebaeac64a6b8a17
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2022-12-06 11:09:23 +0100

    Use external text file containing Cypher query

--- cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/TimeManipulationCheck.kt
+++ cpg-solidity/src/main/kotlin/de/fraunhofer/aisec/cpg/checks/TimeManipulationCheck.kt
@@ -1,22 +1,18 @@
 package de.fraunhofer.aisec.cpg.checks
 
 import de.fraunhofer.aisec.cpg.sarif.PhysicalLocation
-import org.neo4j.driver.Record
 import org.neo4j.driver.Transaction
 
-class TimeManipulationCheck: Check() {
+class TimeManipulationCheck : Check() {
 
     override fun getVulnerabilityName(): String {
         return "Time Manipulation Vulnerability"
     }
 
     override fun check(transaction: Transaction): List<PhysicalLocation> {
-        var findings: MutableList<PhysicalLocation> = mutableListOf()
-        val query = """
-            MATCH p=(n:DeclaredReferenceExpression)-[:DFG*]->(m) -[:EOG*]->(l:CallExpression)
-            WHERE n.name = "timestamp" AND l.name = "send"
-            RETURN distinct n as time, n.startLine as sline, n.endLine as eline, n.startColumn as scol, n.endColumn as ecol, n.artifact as file
-        """.trimIndent()
+        val findings: MutableList<PhysicalLocation> = mutableListOf()
+        val query = object {}.javaClass.getResourceAsStream("/TimeManipulation")?.bufferedReader()?.readText()
+
         transaction.run(query).let { result ->
             while (result.hasNext()) {
                 val row: Map<String, Any> = result.next().asMap()
