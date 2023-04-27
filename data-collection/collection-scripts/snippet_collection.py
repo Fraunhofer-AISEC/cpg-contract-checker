59a7011abd528605364c744f6a818bc9a469dbb0
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2023-04-27 14:23:59 +0200

    Updating statistics script

--- data-collection/collection-scripts/snippet_collection.py
+++ data-collection/collection-scripts/snippet_collection.py
@@ -25,6 +25,12 @@ vulnerable_contracts = {}
 
 clone_checks = {}
 
+contract_creation_times = {}
+
+
+of = 0
+excludenr = 0
+
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
@@ -46,14 +52,45 @@ def parse_vulnerabillities(dirname, filename):
     
     
 def collect_timestamps():
+    timestamp_mapping= {}
     with open("stack_overflow_posts.json") as f:
         line = f.readline()
         while line:
             sos = json.loads(line)
-            
-            print(sos)
+            if sos["question_id"] in timestamp_mapping:
+                print("Dublicate snippet entries for: " + str(sos["question_id"]))
+            timestamp_mapping[sos["question_id"]] = sos["creation_date"]
             
             line = f.readline()
+    with open("ethereum_stack_exchange_posts.json") as f:
+        line = f.readline()
+        while line:
+            sos = json.loads(line)
+            if sos["question_id"] in timestamp_mapping:
+                print("Dublicate snippet entries for: " + str(sos["question_id"]))
+            timestamp_mapping[sos["question_id"]] = sos["creation_date"]
+                    
+            line = f.readline()
+        with open("smart_contract_sanctuary_timestamps.json") as f:
+            line = f.readline()
+            while line:
+                sos = json.loads(line)
+                address = sos["contractAddress"].lower()
+                if address in contract_creation_times:
+                    print("Dublicate contract entries for: " + address)
+                contract_creation_times[address] = sos["timestamp"]
+                        
+                line = f.readline()
+    for k,v in vulnerable_contracts.items():
+        question_id = int(k.split("_")[0])
+        if question_id in timestamp_mapping:
+            vulnerable_contracts[k]["timestamp"] = timestamp_mapping[question_id]
+        else:
+            print("Sample without timestamp: " + str(k))
+    # print(timestamp_mapping)
+        
+            
+
             
 def collect_clone_mapping(snippet_to_contract):
     for root, d_names, f_names in os.walk(snippet_to_contract):
@@ -71,6 +108,22 @@ def collect_clone_mapping(snippet_to_contract):
                             vulnerable_contracts[snippet_file]["clones"].append(sos)
                             
                         line = mapping.readline()
+
+                    
+def eliminate_impossible_causal_clones():
+    global of, excludenr
+    for k, v in vulnerable_contracts.items():
+        incorrect_clones = []
+        of += len(v["clones"])
+        for clone in v["clones"]:
+            clone_address = "0x" + os.path.basename(clone).split("_")[0].lower()
+            if clone_address in contract_creation_times:
+                if v["timestamp"] >= contract_creation_times[clone_address]:
+                    excludenr += 1
+                    incorrect_clones.append(clone)
+        v["clones"] = [clone for clone in v["clones"] if clone not in incorrect_clones]
+    
+                    
                 
 
 def collect_statistics():
@@ -94,14 +147,20 @@ for root, d_names, f_names in os.walk(path):
             fname.append(os.path.join(root, f))
             parse_vulnerabillities(root, f)
             snippets_with_results += 1
+print("Number of snippets: " + str(snippets_with_results))
+print("Number of vulnerbale snippets: " + str(len(vulnerable_contracts)))
+collect_timestamps()
 collect_clone_mapping(sys.argv[2] + "/ethereum_stack_exchange_matches" )
 collect_clone_mapping(sys.argv[2] + "/stack_overflow_matches")
+print("Of which snippets have clones: " + str(len([snip for snip in vulnerable_contracts if vulnerable_contracts[snip]["clones"]])))
+eliminate_impossible_causal_clones()
+print("After filtering for timestamp " + str(len([snip for snip in vulnerable_contracts if vulnerable_contracts[snip]["clones"]])) + " of clones remain.")
 collect_statistics()
 # TODO Print statistics
 # Number of snippets with vuln, number of snippets that also have a vulnerable clone.
 # avg number of checks to rerun per clone file, appearances of each vulnerbaility in vuln snippets
-print("Number of snippets: " + str(snippets_with_results))
-print("Number of vulnerbale snippets: " + str(len(vulnerable_contracts)))
+
+print(str(excludenr) + " of " + str(of) +" clone relations were not valid")
 print("Number of clones to analyze: " + str(len(clone_checks)))
 nr = 0
 for clone, checks in clone_checks.items():
